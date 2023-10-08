d31a1b5e05ae323f5c3eddd7ef013ead3ade53ab
Bob Arrington <rarrington@sincerelyhers.com>
2023-10-08 19:50:27 -0400

    [WIP]

--- fake_plpy/__init__.py
+++ fake_plpy/__init__.py
@@ -3,21 +3,19 @@ import logging
 
 import psycopg2
 from psycopg2 import sql
-from psycopg2.extensions import quote_ident
 
 
 class PlPy(object):
-
     def __init__(self, dsn=None, logger=None):
         if logger is None:
             logger = logging.getLogger(__name__)
         self.logger = logger
         if dsn is None:
-            dsn = ''
+            dsn = ""
         self.dsn = dsn
         self.db = psycopg2.connect(dsn=self.dsn)
         self.cursor = self.db.cursor()
-        self.ident = quote_ident(value=None, scope=None)
+        self.ident = sql.Identifier
         self.literal = sql.Literal
 
     def notice(self, message):
@@ -41,19 +39,14 @@ class PlPy(object):
     def fatal(self, message):
         self.logger.info(message)
 
-
     def execute(self, *args, **kwargs):
         return self.cursor.execute(*args, **kwargs)
 
     def prepare(self, statement):
         return self.execute(f"PREPARE {statement}")
 
-
-    def quote_literal(self, *args, **kwargs):
-        return self.literal(*args, **kwargs)
+    def quote_literal(self, value):
+        return self.literal(value)
 
     def quote_ident(self, value):
-        scope = self.cursor
-        if self.cursor is None:
-            scope = self.db
-        return self.ident(value, scope)
+        return self.ident(value)
